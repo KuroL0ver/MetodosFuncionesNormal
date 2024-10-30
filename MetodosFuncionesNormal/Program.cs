c6024b72f7a304102bf7dd5f083fae3e0d47fb34
Camila Fimbres <camifimbres@gmail.com>
2024-10-30 08:02:41 -0700

    Agregar archivos de proyecto.

---
+++ MetodosFuncionesNormal/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MetodosFuncionesNormal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje("Mensaje Desde el Método...1");
+            MostrarMensaje("Mensaje Desde el Método...2");
+
+            Sumar(2, 5);
+            Sumar(3, 9);
+        }
+
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        static void Sumar(int primerNumero, int segundoNumero)
+        {
+            int total = primerNumero + segundoNumero;
+            MostrarMensaje(total.ToString());
+        }
+    }
+}
